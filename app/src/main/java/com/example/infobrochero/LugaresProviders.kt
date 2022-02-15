abc25f2e3e92613a076076e8827db7861020f244
EstebanLes <estebanles22@gamail.com>
2022-02-15 17:33:32 -0300

    providers

--- app/src/main/java/com/example/infobrochero/LugaresProviders.kt
+++ app/src/main/java/com/example/infobrochero/LugaresProviders.kt
@@ -10,17 +10,17 @@ class LugaresProviders {
         ,"cartel bienvenidos "
         ,"https://www.google.com/search?q=bienvenidos+a+cura+brochero+cordoba&sxsrf=APq-WBt6_VXvPCQgyZLkVGyZ2fxtSDecYQ:1644956101479&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiIvuWAw4L2AhUrD7kGHQ9eACAQ_AUoAXoECAEQAw&biw=1536&bih=722&dpr=1.25#imgrc=9jqFsbc7BBjBqM"
         ),
-        Lugar( "Balneario"
-        ,"Balneario de cura brochero se encuentra en el cruce con San Lorenzo"
-        ,"balneario.jpg"
+        Lugar( "Iglesia"
+        ,"Iglesia principal de villa cura brochero"
+        ,"https://www.google.com/search?q=iglesia+de+cura+brochero+cordoba&tbm=isch&ved=2ahUKEwiZh9fJw4L2AhXULbkGHUeECWoQ2-cCegQIABAA&oq=iglesia+de+cura+brochero+cordoba&gs_lcp=CgNpbWcQDDIECAAQGDoGCAAQBxAeULULWIgXYJsqaABwAHgAgAFZiAGPBZIBATiYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=XgoMYpngE9Tb5OUPx4im0AY&bih=722&biw=1536#imgrc=wIJ__qEV4nn9EM"
         ),
-        Lugar( "Balneario"
-        ,"Balneario de cura brochero se encuentra en el cruce con San Lorenzo"
-        ,"balneario.jpg"
+        Lugar( "Las Maravillas"
+        ,"Balneario natural de aguas cristalinas"
+        ,"https://www.google.com/search?q=las+maravillas+de+cura+brochero+cordoba&tbm=isch&ved=2ahUKEwj-0vuExoL2AhW1DrkGHa-UDpEQ2-cCegQIABAA&oq=las+maravillas+de+cura+brochero+cordoba&gs_lcp=CgNpbWcQAzoHCCMQ7wMQJ1DaC1i-LmCSRWgAcAB4AIABVogBkQmSAQIxNZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=8wwMYr6NILWd5OUPr6m6iAk&bih=722&biw=1536#imgrc=QalWBB8FO-QyTM"
         ),
-        Lugar( "Balneario"
-        ,"Balneario de cura brochero se encuentra en el cruce con San Lorenzo"
-        ,"balneario.jpg"
+        Lugar( "La Toma"
+        ,"Balneario donde se encuentra un tobogan natural de piedras "
+        ,"https://www.google.com/search?q=la+toma+de+cura+brochero+cordoba&tbm=isch&ved=2ahUKEwjgxYKcxoL2AhW_ALkGHYGBA4kQ2-cCegQIABAA&oq=la+toma+de+cura+brochero+cordoba&gs_lcp=CgNpbWcQDDoHCCMQ7wMQJ1CdGFjANmDxR2gAcAB4AIAB3QGIAaIMkgEGMTcuMC4xmAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=Iw0MYqCoNb-B5OUPgYOOyAg&bih=722&biw=1536#imgrc=UrAHTId9ngFNgM"
         )
     )
 }
