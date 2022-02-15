8993edfe826220fff48d3a758389cd88a2b5e2d6
EstebanLes <estebanles22@gamail.com>
2022-02-15 17:18:56 -0300

    Initial commit

---
+++ app/src/main/java/com/example/infobrochero/Lugares.kt
@@ -0,0 +1,8 @@
+package com.example.infobrochero
+
+data class Lugar (
+    val nombre:String,
+    val descripcion:String,
+    val foto:String){
+
+}
---
+++ app/src/main/java/com/example/infobrochero/LugaresProviders.kt
@@ -0,0 +1,26 @@
+package com.example.infobrochero
+
+class LugaresProviders {
+    val ListaLugares = listOf<Lugar>(
+        Lugar( "Balneario"
+            ,"Balneario de cura brochero se encuentra en el cruce con San Lorenzo"
+            ,"https://www.google.com/search?q=balneario+de+cura+brochero+cordoba&tbm=isch&ved=2ahUKEwjS1smDw4L2AhXcNbkGHeMADtcQ2-cCegQIABAA&oq=balneario+de+cura+brochero+cordoba&gs_lcp=CgNpbWcQAzoHCCMQ7wMQJ1D7CliwNGCyNmgCcAB4AIABV4gBpAmSAQIxNZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=ywkMYpLFEtzr5OUP44G4uA0&bih=722&biw=1536#imgrc=i4Ey50jMsmNCyM"
+        ),
+        Lugar( "Bienvenidos"
+        ,"cartel bienvenidos "
+        ,"https://www.google.com/search?q=bienvenidos+a+cura+brochero+cordoba&sxsrf=APq-WBt6_VXvPCQgyZLkVGyZ2fxtSDecYQ:1644956101479&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiIvuWAw4L2AhUrD7kGHQ9eACAQ_AUoAXoECAEQAw&biw=1536&bih=722&dpr=1.25#imgrc=9jqFsbc7BBjBqM"
+        ),
+        Lugar( "Balneario"
+        ,"Balneario de cura brochero se encuentra en el cruce con San Lorenzo"
+        ,"balneario.jpg"
+        ),
+        Lugar( "Balneario"
+        ,"Balneario de cura brochero se encuentra en el cruce con San Lorenzo"
+        ,"balneario.jpg"
+        ),
+        Lugar( "Balneario"
+        ,"Balneario de cura brochero se encuentra en el cruce con San Lorenzo"
+        ,"balneario.jpg"
+        )
+    )
+}
